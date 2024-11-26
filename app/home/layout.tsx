b0dc70d319cac2ebed40187b19ecdedde5d28182
Ryan Neil Abina <your_email@example.com>
2024-11-26 15:27:25 +0800

    refactor layout for resource category

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,4 +1,3 @@
-import AppProvider from '@/providers/AppProvider';
 import { Header } from '@/components/landing/Header';
 import { Footer } from '@/components/reusable';
 export default function RootLayout({
@@ -7,22 +6,10 @@ export default function RootLayout({
     children: React.ReactNode;
   }) {
     return (
-      <html lang="en">
-        <head>
-          {/* <ColorSchemeScript /> */}
-          <link rel="shortcut icon" href="/favicon.svg" />
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-          />
-        </head>
-        <body>
-          <AppProvider>
-            <Header />
-              {children}
-            <Footer />
-          </AppProvider>
-        </body>
-      </html>
+      <>
+        <Header />
+          {children}
+        <Footer />
+      </>
     );
-  }
+}
